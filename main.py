054d63339fb0b049f4ad2ad4a51683776344bb11
Vitalii Bilyk <smstur@gmail.com>
2024-06-17 15:26:10 +0200

    Add redirect

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 import schemas
 
 app = FastAPI()
 
-# TODO:
-# GET redirect if url in the list
-# POST create a new record
-
 @app.get("/{short_str}")
 def read_item(short_str: str):
-    # TODO: check if exist, increase redirect_count, and redirect
-    return {"item_id": short_str}
+    # TODO: check if exist, and redirect
+    if short_str == "123":
+        # TODO: increase redirect_count
+        return RedirectResponse("https://google.com/")
+    else:
+        raise HTTPException(status_code=404, detail="Redirect not found")
 
 @app.post("/redirects")
 def crete_redirect(redirect: schemas.Redirect):
